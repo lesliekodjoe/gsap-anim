c088b762837d8dbe7b08a80533433f1e3ffebd45
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-10-03 11:33:58 +0000

    Navbar setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,18 +3,21 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <ul className="flex space-x-4 text-white">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+    <nav className="flex justify-between mx-10">
+      <Link to="/">
+        <div className="w-[8em]">
+          <img
+            src="https://assets.codepen.io/2621168/logo_1.svg"
+            className="w-full h-full"
+            alt="SkillForm Logo"
+          ></img>
+        </div>
+      </Link>
+      <div className="inline-flex">
+        <Link to="/">Home</Link>
+        <Link to="/Contact">Contact</Link>
+        <Link to="/Footer">Footer</Link>
+      </div>
     </nav>
   );
 };
