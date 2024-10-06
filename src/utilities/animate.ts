d4e4dcf846c10c737d39a3ba2025d50eac7938d3
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-10-06 12:59:37 +0000

    Updates

--- src/utilities/animate.ts
+++ src/utilities/animate.ts
@@ -25,12 +25,24 @@ export const imgFadeIn = (target: GsapTarget) => {
     scrollTrigger: {
       trigger: target,
       scrub: 1,
+      start: "top 80%",
     },
   });
 
   //   first parameter is selector, second=cssStyle
   // Adding the .to() after the first t1.to allows me to chain the animation
-  tl.to(target, {
-    clipPath: "polygon(7% 7%, 93% 7%, 93% 93%, 7% 93%)",
-  });
+  // tl.to(target, {
+  //   clipPath: "polygon(7% 7%, 93% 7%, 93% 93%, 7% 93%)",
+  // });
+    tl.fromTo(
+      target,
+      {
+        clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+        borderRadius: "0px",
+      }, // Initial state
+      {
+        clipPath: "polygon(7% 7%, 93% 7%, 93% 93%, 7% 93%)",
+        borderRadius: "200px",
+      } // Final state
+    );
 };
